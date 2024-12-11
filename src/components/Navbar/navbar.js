59a1f48745dcf6d16547eca3533cb80e697ae43a
jonastino21 <heryniavo@yahoo.com>
2024-12-11 09:00:19 +0000

    ✨

--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -9,7 +9,7 @@ const Navbar = ({ openModal }) => {
             <li><a href="#Apropos">About Me</a></li>
             <li><a href="#CarteVisite" onClick={openModal} >Generate a business card</a></li>
             <li><a href="#Contact">Contact Me</a></li>
-            <li><a href="https://jonastino21.github.io/Jonastino_cv/">My CV</a></li> 
+            <li><a href="https://docs.google.com/document/d/1mJp965EWkc1H154GbDfad7YAcD_PbjCYdZ4I7N-jrQY/edit?usp=sharing">My CV</a></li> 
         </nav>
         </header>
     ) 
